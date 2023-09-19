046d3ab67fff6e997c2162f07d2891b0e4609a56
Peipr <maro@peiphy.xyz>
2023-09-19 13:32:10 +0200

    New update?

--- src/components/Frame1/Frame1.tsx
+++ src/components/Frame1/Frame1.tsx
@@ -2,8 +2,9 @@ import { memo } from 'react';
 import type { FC } from 'react';
 
 import resets from '../_resets.module.css';
-import { DesktopTemplateIcon } from './DesktopTemplateIcon.js';
 import classes from './Frame1.module.css';
+import { MoonIcon } from './MoonIcon.js';
+import { StarsIcon } from './StarsIcon.js';
 
 interface Props {
   className?: string;
@@ -13,7 +14,13 @@ export const Frame1: FC<Props> = memo(function Frame1(props = {}) {
   return (
     <div className={`${resets.storybrainResets} ${classes.root}`}>
       <div className={classes.desktopTemplate}>
-        <DesktopTemplateIcon className={classes.icon} />
+        <div className={classes.moon}>
+          <MoonIcon className={classes.icon} />
+        </div>
+        <div className={classes.stars}>
+          <StarsIcon className={classes.icon2} />
+        </div>
+        <div className={classes.myNewPersonalWebsiteIsComingSo}>My new personal website is coming soon!</div>
       </div>
     </div>
   );
