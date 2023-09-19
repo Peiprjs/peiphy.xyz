c21549f91cd4a266d5ec0b0beb0401992cde1102
Peipr <maro@peiphy.xyz>
2023-09-19 10:18:59 +0200

    First test importing from Figma via React

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import cn from 'classnames';
+import styles from './Frontpage.module.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Frontpage(props) {
+  return (
+    <div
+      className={cn(styles.root, 'frontpage')}
+      style={{ '--src': `url(${require('assets/2e2b869a097e07a376cd4a9b3aab5b8e.png')})` }}
+    />
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default Frontpage;
+
